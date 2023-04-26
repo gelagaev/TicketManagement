5d265b7573eb2bb7d78f5e12aabdc52152004b7d
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-04-26 03:51:24 +0300

    Add JWT options and configuration

---
+++ Server/src/Core/Configurations/JwtConfig.cs
@@ -0,0 +1,48 @@
+using System.Text;
+using Core.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Core.Configurations;
+
+/// <summary>
+/// Authentication and JwtBearer configuration
+/// </summary>
+public static class JwtConfig
+{
+  /// <summary>
+  /// Adds Authentication and JwtBearer configuration
+  /// </summary>
+  /// <param name="builder">App builder</param>
+  public static void ConfigureBearerAuth(this WebApplicationBuilder builder)
+  {
+    var authTokenSettingsSection = builder.Configuration.GetSection(nameof(AuthTokenOptions));
+    var options = authTokenSettingsSection.Get<AuthTokenOptions>();
+    if (options == null)
+      return;
+    builder.Services.AddAuthentication(opt =>
+      {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+      })
+      .AddJwtBearer(opt =>
+      {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuer = true,
+          ValidateAudience = true,
+          ValidateLifetime = true,
+          ValidateIssuerSigningKey = true,
+          ValidIssuer = options.Issuer,
+          ValidAudience = options.Audience,
+          IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.SecretKey))
+        };
+      });
+    
+    builder.Services.Configure<AuthTokenOptions>(authTokenSettingsSection);
+  }
+}
---
+++ Server/src/Core/Options/AuthTokenOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Options;
+
+public class AuthTokenOptions
+{
+  public string Issuer { get; init; } = string.Empty;
+  public string Audience { get; init; } = string.Empty;
+  public string SecretKey { get; init; } = string.Empty;
+  public int LifeTimeHours { get; init; }
+}
