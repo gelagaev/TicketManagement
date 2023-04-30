52f3c172e7d36d983a6ff52e93a38fd7de7b21ba
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-04-30 10:27:48 +0300

    Add comment constructor

--- Server/src/Core/TicketAggregate/Comment.cs
+++ Server/src/Core/TicketAggregate/Comment.cs
@@ -4,8 +4,14 @@ namespace Core.TicketAggregate;
 
 public class Comment : EntityBase<Guid>
 {
-  public string CommentText { get; set; } = string.Empty;
-  public Guid UserId { get; set; }
+  public Guid UserId { get; private set; }
+  public string CommentText { get; private set; }
+
+  public Comment(Guid userId, string commentText)
+  {
+    UserId = userId;
+    CommentText = commentText;
+  }
 
   public override string ToString()
   {
