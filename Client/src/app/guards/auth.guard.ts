6c72239f2d54f7ab7f0186a81f72963bcab7390f
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-05-04 03:32:23 +0300

    Introduce auth guard

--- Client/src/app/guards/auth.guard.ts
+++ Client/src/app/guards/auth.guard.ts
@@ -2,6 +2,4 @@ import { CanActivateFn } from '@angular/router';
 import { inject } from "@angular/core";
 import { PermissionsService } from "../services/permissions.service";
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return inject(PermissionsService).canActivateAuth();
-};
+export const authGuard: CanActivateFn = () => inject(PermissionsService).canActivateAuth();
