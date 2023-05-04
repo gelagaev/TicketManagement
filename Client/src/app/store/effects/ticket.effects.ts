26f67286b89777bf893be40189502088e49bda47
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-05-04 03:40:54 +0300

    Introduce ticket effects

---
+++ Client/src/app/store/effects/ticket.effects.ts
@@ -0,0 +1,32 @@
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { catchError, exhaustMap, map, of } from "rxjs";
+import { BackendError } from "../../interceptors/http-request-failure.interceptor";
+import { Injectable } from "@angular/core";
+import { ServiceProxy } from "../../services/web-api-service-proxies";
+import { TicketActions } from "../actions";
+
+@Injectable()
+export class TicketEffects {
+  constructor(private actions$: Actions, private serviceProxy: ServiceProxy,) {
+  }
+
+  load$ = createEffect(() => {
+      return this.actions$.pipe(
+        ofType(TicketActions.loadTickets),
+        exhaustMap((payload) => {
+          return this.serviceProxy.ticket_List(undefined)
+            .pipe(
+              map(response => TicketActions.loadTicketSuccess({tickets: response.tickets!})), //todo
+              catchError((error: BackendError) => of(TicketActions.loadTicketFailure(error)))
+            );
+        })
+      );
+    }
+  );
+
+  loadSuccess$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(TicketActions.loadTicketSuccess),
+      map(response => TicketActions.loadTicketSuccess({tickets: response.tickets}))
+    ), {dispatch: false});
+}
