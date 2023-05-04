f649dabc8f6b4e5f436abedff6bbd713cbd23705
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-05-04 07:00:25 +0300

    Introduce ticket-list.component

--- Client/src/app/components/ticket-list/ticket-list.component.ts
+++ Client/src/app/components/ticket-list/ticket-list.component.ts
@@ -4,12 +4,15 @@ import { select, Store } from "@ngrx/store";
 import { TicketActions } from "../../store/actions";
 import { TicketRecord } from "../../services/web-api-service-proxies";
 import { selectAllTickets } from "../../store/reducers";
+import { TicketListItemComponent } from "../ticket-list-item/ticket-list-item.component";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
 
 
 @Component({
   selector: 'tm-ticket-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TicketListItemComponent, MatListModule, MatCardModule],
   templateUrl: './ticket-list.component.html',
   styleUrls: ['./ticket-list.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
