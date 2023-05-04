3b1cb33a3572a8b9afb751553b2d6410ac704dbf
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-05-04 03:42:22 +0300

    Introduce navigation actions

---
+++ Client/src/app/store/actions/navigation.actions.ts
@@ -0,0 +1,4 @@
+import { createAction } from "@ngrx/store";
+
+export const navigateToSignIn = createAction('[Navigation] SignIn');
+export const navigateToRegister = createAction('[Navigation] Register');
