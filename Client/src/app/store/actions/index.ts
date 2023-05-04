b9e1c5eb1f0b9f001e3928d33e135c6e2bdf8e91
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-05-04 03:42:49 +0300

    Introduce index for actions

---
+++ Client/src/app/store/actions/index.ts
@@ -0,0 +1,9 @@
+import * as AuthActions from "./auth.actions";
+import * as NavigationActions from "./navigation.actions";
+import * as TicketActions from "./ticket.actions";
+
+export {
+  AuthActions,
+  NavigationActions,
+  TicketActions,
+};
