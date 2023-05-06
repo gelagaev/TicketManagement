108fa83d4521615ff1ae0b2cb725e1d4b3a9d7be
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-05-06 15:10:36 +0300

    Update ticket-comment-list-item.component

--- Client/src/app/components/ticket-comment-list/ticket-comment-list.component.ts
+++ Client/src/app/components/ticket-comment-list/ticket-comment-list.component.ts
@@ -1,16 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { select, Store } from "@ngrx/store";
+import { CommentRecord } from "../../services/web-api-service-proxies";
+import { isEditingComment, selectAllComments } from "../../store/reducers/index.comment";
+import { CommentActions } from "../../store/actions";
+import { TicketListItemComponent } from "../ticket-list-item/ticket-list-item.component";
+import { TicketCommentListItemComponent } from "../ticket-comment-list-item/ticket-comment-list-item.component";
 
 @Component({
   selector: 'tm-ticket-comment-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TicketListItemComponent, TicketCommentListItemComponent],
   templateUrl: './ticket-comment-list.component.html',
   styleUrls: ['./ticket-comment-list.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TicketCommentListComponent {
-  constructor() {
-    console.log("TicketCommentListComponent");
+export class TicketCommentListComponent implements OnInit {
+  @Input()
+  ticketId: string = "";
+
+  public comments$ = this.store.pipe(select(selectAllComments));
+
+  constructor(private store: Store<CommentRecord>) {
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(CommentActions.loadTicketComment({ticketId: this.ticketId}));
+  }
+
+  public trackByFn(index: number, {id}: CommentRecord): string {
+    return id;
+  }
+
+  public isCommentEditing(id: string) {
+    return this.store.pipe(select(isEditingComment(id)))
   }
 }
