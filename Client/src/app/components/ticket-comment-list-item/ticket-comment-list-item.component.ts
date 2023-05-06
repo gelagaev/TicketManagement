8e6f229f51265cf703ea4602ebefdb63601af74b
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-05-06 15:11:08 +0300

    Update ticket-comment-list-item-item.component

--- Client/src/app/components/ticket-comment-list-item/ticket-comment-list-item.component.ts
+++ Client/src/app/components/ticket-comment-list-item/ticket-comment-list-item.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommentRecord } from "../../services/web-api-service-proxies";
+import { ControlErrorsPipe } from "../../pipes/control-errors.pipe";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { Store } from "@ngrx/store";
+import { CommentActions } from "../../store/actions";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'tm-ticket-comment-list-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ControlErrorsPipe, MatCardModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule, MatIconModule],
   templateUrl: './ticket-comment-list-item.component.html',
-  styleUrls: ['./ticket-comment-list-item.component.less']
+  styleUrls: ['./ticket-comment-list-item.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TicketCommentListItemComponent {
+  @Input()
+  comment: CommentRecord = new CommentRecord();
 
+  @Input()
+  isEditing = false;
+
+  private get commentId(): string  { return this.comment.id; }
+
+  form = new FormGroup({
+    commentText: new FormControl<string>('', [Validators.required, Validators.maxLength(1000)]),
+  })
+
+  constructor(private store: Store<CommentRecord>) {
+  }
+
+  onEdit(): void {
+    this.store.dispatch(CommentActions.startEditTicketComment({commentId: this.comment.id}));
+    this.form.controls.commentText.setValue(this.comment.commentText!);
+  }
+
+  onDelete(): void {
+    if (window.confirm("Delete comment?")) {
+      this.store.dispatch(CommentActions.deleteTicketComment({commentId: this.commentId}));
+    }
+  }
+
+  onSave(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.store.dispatch(CommentActions.updateTicketComment({
+      id: this.comment.id,
+      commentText: this.form.controls.commentText.value!,
+    }));
+  }
+
+  onCancel(): void {
+    this.store.dispatch(CommentActions.endEditTicketComment({commentId: this.commentId}))
+  }
 }
