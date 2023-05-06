38eb46c7b20c895e18a0195975f2e372a8cd0fff
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-05-06 15:00:51 +0300

    Introduce UpdateCommentHandler

---
+++ Server/src/WebApi/Handlers/TicketHandlers/UpdateCommentHandler.cs
@@ -0,0 +1,32 @@
+using Ardalis.GuardClauses;
+using Core.TicketAggregate;
+using Core.TicketAggregate.Specifications;
+using Kernel.Interfaces;
+using MediatR;
+using WebApi.Endpoints.TicketEndpoints.V1;
+
+namespace WebApi.Handlers.TicketHandlers;
+
+internal sealed class UpdateCommentHandler : IRequestHandler<UpdateTicketCommentRequest, UpdateTicketCommentResponse>
+{
+  private readonly IRepository<Comment> _repository;
+
+  public UpdateCommentHandler(IRepository<Comment> repository) => _repository = repository;
+
+  public async Task<UpdateTicketCommentResponse> Handle(UpdateTicketCommentRequest request, CancellationToken ct)
+  {
+    var spec = new CommentWithAuthorSpec(request.Id);
+    var existingComment = await _repository.FirstOrDefaultAsync(spec, ct);
+    Guard.Against.Null(existingComment, nameof(existingComment));
+
+    existingComment.UpdateCommentText(request.CommentText);
+
+    await _repository.UpdateAsync(existingComment, ct);
+
+    var response = new UpdateTicketCommentResponse(
+      comment: new CommentRecord(existingComment.Id, existingComment.CommentText, existingComment.Author.Id, existingComment.Author.FullName)
+    );
+
+    return response;
+  }
+}
