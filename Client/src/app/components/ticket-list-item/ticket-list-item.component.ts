ae0e736d1519f0fefca73f4455745d38d6fd9694
Sergey Gelagaev <sgelagaev@it-one.ru>
2023-05-07 21:11:45 +0300

    Update ticket-list-item.component

--- Client/src/app/components/ticket-list-item/ticket-list-item.component.ts
+++ Client/src/app/components/ticket-list-item/ticket-list-item.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CommentRecord, TicketRecord } from "../../services/web-api-service-proxies";
+import { CommentRecord, TicketRecord, UserRecord } from "../../services/web-api-service-proxies";
 import { MatCardModule } from "@angular/material/card";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatListModule } from "@angular/material/list";
@@ -15,20 +15,23 @@ import { Actions, ofType } from "@ngrx/effects";
 import { CommentActions, TicketActions } from "../../store/actions";
 import { tap } from "rxjs";
 import { AddTicketCommentComponent } from "../add-ticket-comment/add-ticket-comment.component";
-import { Store } from "@ngrx/store";
+import { select, Store } from "@ngrx/store";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { selectAllUsers } from "../../store/reducers/user.index";
+import { MatOptionModule } from "@angular/material/core";
+import { MatSelectModule } from "@angular/material/select";
 
 @UntilDestroy()
 @Component({
   selector: 'tm-ticket-list-item',
   standalone: true,
-  imports: [CommonModule, MatCardModule, ReactiveFormsModule, MatListModule, MatIconModule, MatButtonModule, ControlErrorsPipe, MatFormFieldModule, MatInputModule, MatExpansionModule, TicketCommentListComponent, AddTicketCommentComponent],
+  imports: [CommonModule, MatCardModule, ReactiveFormsModule, MatListModule, MatIconModule, MatButtonModule, ControlErrorsPipe, MatFormFieldModule, MatInputModule, MatExpansionModule, TicketCommentListComponent, AddTicketCommentComponent, MatOptionModule, MatSelectModule],
   templateUrl: './ticket-list-item.component.html',
   styleUrls: ['./ticket-list-item.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class TicketListItemComponent {
+export class TicketListItemComponent implements AfterViewInit {
   constructor(private actions$: Actions, private store: Store<CommentRecord>) {
     this.actions$.pipe(
       untilDestroyed(this),
@@ -44,14 +47,23 @@ export class TicketListItemComponent {
 
   @Input()
   isAuthor: boolean | null = null;
+  @Input()
+  isAdmin: boolean | null = null;
 
   isEdit = false;
+
+  users$ = this.store.pipe(select(selectAllUsers));
 
   form = new FormGroup({
     subject: new FormControl<string>('', [Validators.required, Validators.maxLength(100)]),
     description: new FormControl<string>('', [Validators.required, Validators.maxLength(1000)]),
   })
   public isShowComments = false;
+  selectedManagerId: string = "-1";
+
+  public trackByFn(index: number, {id}: UserRecord): string {
+    return id;
+  }
 
   onEdit(): void {
     this.form.controls.subject.setValue(this.ticket.subject ?? "");
@@ -85,5 +97,35 @@ export class TicketListItemComponent {
     this.store.dispatch(
       CommentActions.createTicketComment({commentText, ticketId: this.ticket.id})
     );
+  }
+
+  showSaveAssignButton(): boolean {
+    if (this.selectedManagerId === '-1') {
+      return !!this.ticket.assignId
+    } else {
+      return this.selectedManagerId !== this.ticket.assignId;
+    }
+  }
+
+  onAssignManager(): void {
+    this.store.dispatch(TicketActions.assignTicket({
+      managerId: this.selectedManagerId === '-1' ? undefined : this.selectedManagerId,
+      ticketId: this.ticket.id
+    }));
+  }
+
+  ngAfterViewInit(): void {
+    if (this.ticket.assignId) {
+      this.selectedManagerId = this.ticket.assignId;
+    }
+  }
+
+  protected readonly onclose = onclose;
+
+  onClose(): void {
+    if (window.confirm("Close ticket?")) {
+      this.store.dispatch(TicketActions.closeTicket({ticketId: this.ticket.id}));
+
+    }
   }
 }
